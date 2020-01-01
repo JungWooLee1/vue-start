d1fafac4df1659da3aaf374bf795a58596de9819
jungwoole91 <jungwoolee91@gmail.com>
2020-01-02 04:42:50 +0900

    지뢰찾기 완료 / Vue router 완료 (완강)

--- 8.지뢰찾기/MineForm.vue
+++ 8.지뢰찾기/MineForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <input type="number" placeholder="세로" :value="row" @change="onChangeRow" />  <!--v-on:click => @click -->
-    <input type="number" placeholder="가로" :value="row" @change="onChangeCell" />
+    <input type="number" placeholder="가로" :value="row" @change="onChangeRow" />  <!--v-on:click => @click -->
+    <input type="number" placeholder="세로" :value="cell" @change="onChangeCell" />
     <input type="number" placeholder="지뢰" :value="mine" @change="onChangeMine" />
     <button @click="onClickBtn">시작</button>
   </div>
@@ -20,13 +20,13 @@
     },
     methods: {
       onChangeRow(e) {
-
+        this.row = e.target.value;
       },
       onChangeCell(e) {
-
+        this.cell = e.target.value;
       },
       onChangeMine(e) {
-
+        this.mine = e.target.value;
       },
       onClickBtn() {
         this.$store.commit(START_GAME, { row: this.row, cell: this.cell, mine: this.mine});
--- 8.지뢰찾기/store.js
+++ 8.지뢰찾기/store.js
@@ -76,7 +76,9 @@ export default new Vuex.Store({
       mine: 0,
     },
     timer: 0,
+    halted:true, // 게임이 중단
     result: '',
+    openedCount: 0,
   },
   getters: {
 
@@ -96,13 +98,120 @@ export default new Vuex.Store({
 
       state.tableData = plantMine(row, cell, mine);
       state.timer = 0;
+      state.halted = false;
+      state.opendCount = 0;
+      state.result = '';
     },
-    [OPEN_CELL](state){},
-    [CLICK_MINE](state){},
-    [FLAG_CELL](state){},
-    [QUESTION_CELL](state){},
-    [NORMALIZE_CELL](state){},
-    [INCREMENT_TIMER](state){},
+    [OPEN_CELL](state, { row, cell }) {
+      let openedCount = 0;
+      const checked = [];
+      function checkAround(row, cell) { // 주변 8칸 지뢰인지 확인
+        const checkRowOrCellIsUndefined = row < 0 || row >= state.tableData.length || cell < 0 || cell >= state.tableData[0].length;
+        if (checkRowOrCellIsUndefined) { // undefined 방지
+          return;
+        }
+        // mine이나 flag일 경우 검사하지 않음
+        if ([CODE.OPENED , CODE.FLAG, CODE.FLAG_MINE, CODE.QUESTION_MINE, CODE.QUESTION].includes(state.tableData[row][cell])) {
+          return;
+        }
+        // 한번 체크한 칸은 다시 열지 않음
+        if (checked.includes(row + '/' + cell)) {
+          return;
+        } else {
+          checked.push(row + '/' + cell);
+        }
+
+        let around = [];
+        if (state.tableData[row - 1])
+        {
+          around = around.concat([
+            state.tableData[row - 1][cell - 1], state.tableData[row - 1][cell], state.tableData[row - 1][cell + 1]
+          ]);
+        }
+        around = around.concat([
+          state.tableData[row][cell -1], state.tableData[row][cell], state.tableData[row][cell + 1]
+        ]);
+
+        if (state.tableData[row + 1])
+        {
+          around = around.concat([
+            state.tableData[row + 1][cell - 1], state.tableData[row + 1][cell], state.tableData[row + 1][cell + 1]
+          ]);
+        }
+        const counted = around.filter(function(v) {
+          return [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v);
+        });
+        // 주변칸에 지뢰가 하나도 없으면 주변 8칸 열어보고 지뢰가 있으면 숫자 표시하기
+        if (counted.length === 0 && row > -1) {
+          const near = [];
+          if (row -1 > -1) {
+            near.push([row - 1, cell - 1]);
+            near.push([row - 1, cell]);
+            near.push([row - 1, cell + 1]);
+          }
+          near.push([row, cell - 1]);
+          near.push([row, cell + 1]);
+          if (row + 1 < state.tableData.length) {
+            near.push([row + 1, cell - 1]);
+            near.push([row + 1, cell]);
+            near.push([row + 1, cell + 1]);
+          }
+          near.forEach((n) => {
+            if (state.tableData[n[0]][n[1]] !== CODE.OPENED) {
+              checkAround(n[0], n[1]);
+            }
+          })
+        }
+        if (state.tableData[row][cell] === CODE.NORMAL) {
+          openedCount += 1;
+        }
+
+        Vue.set(state.tableData[row], cell, counted.length);
+      }
+      checkAround(row,  cell);
+
+      let halted = false;
+      let result;
+      if (state.data.row * state.data.cell - state.data.mine === state.opendCount + openedCount) { // 가로 * 세로 - 지뢰갯수 = 지금까지 연 칸의 갯수 + 방금 연 갯수
+        halted = true;
+        result = `${state.timer}초만에 승리하셨습니다.`;
+      }
+
+      state.opendCount += openedCount;
+      state.halted = halted;
+      state.result = result;
+    },
+    [CLICK_MINE](state, {row, cell}){
+      state.halted = true;
+      Vue.set(state.tableData[row], cell, CODE.CLICKED_MINE);
+    },
+    [FLAG_CELL](state, { row, cell }) {
+      if (state.tableData[row][cell] === CODE.MINE) {
+        Vue.set(state.tableData[row], cell, CODE.FLAG_MINE);
+      }
+      else {
+        Vue.set(state.tableData[row], cell, CODE.FLAG);
+      }
+    },
+    [QUESTION_CELL](state, {row, cell}){
+      if (state.tableData[row][cell] === CODE.FLAG_MINE) {
+        Vue.set(state.tableData[row], cell, CODE.QUESTION_MINE);
+      }
+      else {
+        Vue.set(state.tableData[row], cell, CODE.QUESTION);
+      }
+    },
+    [NORMALIZE_CELL](state, {row, cell}){
+      if (state.tableData[row][cell] === CODE.QUESTION_MINE) {
+        Vue.set(state.tableData[row], cell, CODE.MINE);
+      }
+      else {
+        Vue.set(state.tableData[row], cell, CODE.NORMAL);
+      }
+    },
+    [INCREMENT_TIMER](state){
+      state.timer +=1;
+    },
 
   },
   actions: {
